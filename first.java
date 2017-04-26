af283cb6177172d8ea91316a6d297c22071a1d47
Sreesa <sreenusankula@gmail.com>
2017-04-26 21:19:31 +0530

    commit

--- first.java
+++ first.java
@@ -2,6 +2,6 @@ class first {
 
 	public static void mai(String args[])
 	{
-		System.out.println("Hello this is first project man");
+		System.out.println("Hello this is first project guys");
 	}
 }
