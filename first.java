6463d53b4998b15c2bb9204b2ca71052becfe22c
Sreesa <sreenusankula@gmail.com>
2017-04-27 12:17:05 +0530

    commit changes

--- first.java
+++ first.java
@@ -2,6 +2,6 @@ class first {
 
 	public static void mai(String args[])
 	{
-		System.out.println("Hello this is first project for webhooks");
+		System.out.println("Hello this is first project for webhooks for test");
 	}
 }
